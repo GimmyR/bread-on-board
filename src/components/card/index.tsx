097749af8c00357a3d4c5f480913a87fa48117f6
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-03 10:04:10 +0300

    Fixing image source and adding link to an unique recipe

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -4,10 +4,10 @@ import Link from "next/link"
 export default function Card({ recipe } : { recipe: Recipe }) {
     return (
         <div className="card col p-0">
-            <img src={`${API_URL}/images/ + ${recipe.image}`} className="card-img-top" alt="recipe image" height="200"/>
+            <img src={`${API_URL}/images/${recipe.image}`} className="card-img-top" alt="recipe image" height="200"/>
             <div className="card-body">
                 <p className="card-text">{ recipe.title }</p>
-                <Link href="#" className="btn btn-success">Voir la recette</Link>
+                <Link href={`/recipe/${recipe.id}`} className="btn btn-success">Voir la recette</Link>
             </div>
         </div>
     )
