ab866dc8f125fd2f63a0f491c42127865cc59fc1
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-03 17:53:49 +0300

    Adding import of bootstrap icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import "./global.css";
 import Bootstrap from "@/components/bootstrap";
 import Header from "@/components/header";
