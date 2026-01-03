861cdec6511404b723a5067e9a4dcce56cfbdb7a
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-03 09:42:21 +0300

    Creating environment variable for API URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import CallToAction from "@/components/call-to-action";
 import RecipeSection from "@/components/recipe-section";
+import { API_URL } from "@/lib/api";
 
 export default async function HomePage() {
-    const response = await fetch("http://localhost:8080/api/recipes");
+    const response = await fetch(`${API_URL}/api/recipes`);
     const recipes: Recipe[] = response.ok ? await response.json() : [];
 
     return (
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,9 +1,10 @@
+import { API_URL } from "@/lib/api"
 import Link from "next/link"
 
 export default function Card({ recipe } : { recipe: Recipe }) {
     return (
         <div className="card col p-0">
-            <img src={"http://localhost:8080/images/" + recipe.image} className="card-img-top" alt="recipe image" height="200"/>
+            <img src={`${API_URL}/images/ + ${recipe.image}`} className="card-img-top" alt="recipe image" height="200"/>
             <div className="card-body">
                 <p className="card-text">{ recipe.title }</p>
                 <Link href="#" className="btn btn-success">Voir la recette</Link>
