4e0c17628031309273404da4b08c97b3d4ba3912
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-04 15:48:29 +0300

    Doing post request to API to sign in and building cookie if success

---
+++ src/actions/sign-in.tsx
@@ -0,0 +1,32 @@
+"use server";
+
+import bobFetch from "@/lib/bob-fetch";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function signIn(formData: FormData) {
+    const response = await bobFetch("/api/sign-in", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ 
+            username: formData.get("username"), 
+            password: formData.get("password") 
+        })
+    });
+
+    if(response.status == 200) {
+
+        const cookieStore = await cookies();
+        cookieStore.set("token", response.data, {
+            httpOnly: true,
+            secure: true,
+            path: "/",
+            sameSite: "none"
+        });
+
+        redirect("/");
+
+    } else return response;
+}
