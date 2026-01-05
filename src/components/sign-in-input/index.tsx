81baf4a219c4191297d04239318f825a9be7c7e0
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-05 19:02:13 +0300

    Creating component for sign in input

---
+++ src/components/sign-in-input/index.tsx
@@ -0,0 +1,20 @@
+type Props = {
+    marginBottom: number,
+    icon: string,
+    type: string,
+    name: string,
+    placeholder: string
+};
+
+export default function SignInInput({ marginBottom, icon, type, name, placeholder }: Props) {
+    return (
+        <div className={`col-12 col-lg-6 mb-${marginBottom}`}>
+            <div className="input-group">
+                <span className="input-group-text">
+                    <i className={`bi bi-${icon}`}></i>
+                </span>
+                <input type={type} name={name} className="form-control" placeholder={placeholder}/>
+            </div>
+        </div>
+    );
+}
