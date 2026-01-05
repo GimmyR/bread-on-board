cdd92def521e91d08275577fb5acc26226595bb8
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-05 07:59:25 +0300

    Moving create recipe form from app directory to components directory

--- src/components/create-recipe-form/index.tsx
+++ src/components/create-recipe-form/index.tsx
@@ -4,15 +4,43 @@ import createRecipe from "@/actions/create-recipe";
 import { RecipeStepForm } from "@/interfaces/recipe-step";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent, useState } from "react";
+import StepForm from "../step-form";
+import SaveRecipeButton from "../save-recipe-button";
+import { RecipeResponse } from "@/interfaces/recipe";
 
-export default function CreateRecipeForm({ title } : { title: string }) {
+type Props = {
+    title?: string,
+    recipe?: RecipeResponse
+};
+
+const createRecipeStep = () => {
+    return { key: Date.now(), text: "" };
+};
+
+const initSteps = (recipe: RecipeResponse | undefined) => {
+    if(!recipe)
+        return useState<RecipeStepForm[]>([ createRecipeStep() ]);
+
+    else {
+        const steps = recipe.steps.map(step => { 
+
+            return { 
+                id: step.id, 
+                text: step.text 
+            };
+
+        }); return useState<RecipeStepForm[]>([...steps]);
+    }
+};
+
+export default function CreateRecipeForm({ title, recipe } : Props) {
     const [error, setError] = useState<string | undefined>();
-    const [steps, setSteps] = useState<RecipeStepForm[]>([ { key: Date.now(), text: "" } ]);
+    const [steps, setSteps] = initSteps(recipe);
     const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
 
     const addStepRightAfter = (index: number) => {
-        steps.splice(index + 1, 0, { key: Date.now(), text: "" });
+        steps.splice(index + 1, 0, createRecipeStep());
         setSteps([...steps]);
     };
 
@@ -36,15 +64,15 @@ export default function CreateRecipeForm({ title } : { title: string }) {
             router.push(`/recipe/${result.data.id}`);
 
         else { 
-            setError(result.data);
+            setError(result.status != 401 ? result.data : "You are not authenticated");
+            setIsLoading(false);
         }
     };
 
     return (
         <form className="d-flex flex-column col-12 col-lg-6 mx-auto mt-0 mt-lg-5 mb-0 mb-lg-5" onSubmit={handleSubmit}>
-            {error != null && <div className="alert alert-danger mb-4" role="alert">
-                {error}
-            </div>}
+            {error != null && 
+            <div className="alert alert-danger mb-5 py-2" role="alert">{error}</div>}
             <div className="mb-4">
                 <label htmlFor="title" className="form-label text-success fw-bold">Titre de la recette</label>
                 <input type="text" className="form-control" name="title" id="title" defaultValue={title}/>
@@ -57,28 +85,9 @@ export default function CreateRecipeForm({ title } : { title: string }) {
                 <label htmlFor="ingredients" className="form-label text-success fw-bold">Les ingrédients de la recette</label>
                 <textarea id="ingredients" name="ingredients" className="form-control"></textarea>
             </div>
-            {steps.map((step, index) => 
-            <div className="d-flex flex-column mb-4" key={step.key}>
-                <label htmlFor={`step-${index + 1}`} className="d-flex flex-row align-items-center form-label text-success fw-bold">
-                    <button type="button" onClick={() => addStepRightAfter(index)} className="btn btn-outline-light border-0 px-0 me-2">
-                        <i className="bi bi-plus-lg text-primary"></i>
-                    </button>
-                    Etape #{ index + 1 }
-                    <button type="button" onClick={() => removeStepAt(index)} className="btn btn-outline-light border-0 px-0 ms-2">
-                        <i className="bi bi-trash text-primary"></i>
-                    </button>
-                </label>
-                <textarea className="form-control" id={`step-${index + 1}`} value={step.text} onChange={(e) => handleChange(e, index)}></textarea>
-            </div>
-            )}
-            <div className="d-flex flex-row justify-content-end align-items-center">
-                {isLoading && <div className="spinner-grow spinner-grow-sm text-secondary me-1" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>}
-                <button type="submit" className={`btn btn-success ms-3${true ? "" : " disabled"}`}>
-                    Enregistrer
-                </button>
-            </div>
+            {steps.map((step, index) =>
+            <StepForm key={step.id ? step.id : step.key} index={index} step={step} addStepRightAfter={addStepRightAfter} removeStepAt={removeStepAt} handleChange={handleChange}/>)}
+            <SaveRecipeButton isLoading={isLoading}/>
         </form>
     );
 }
