d9a1da99c60dca32365e5e63c9b7e6d1ddc69a31
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-05 09:35:39 +0300

    Allowing to do search

--- src/components/search-modal/index.tsx
+++ src/components/search-modal/index.tsx
@@ -1,20 +1,48 @@
-import Link from "next/link"
+"use client";
 
-export default function SearchModal({ recipe } : { recipe: { title: string, ingredients: string } }) {
+import "./style.css";
+import { RecipeResponse } from "@/interfaces/recipe";
+import bobFetch from "@/lib/bob-fetch";
+import Link from "next/link"
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+
+export default function SearchModal() {
+    const [search, setSearch] = useState<string>("");
+    const [recipes, setRecipes] = useState<RecipeResponse[]>([]);
+    const [error, setError] = useState<string | undefined>();
+    const router = useRouter();
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value);
+        searchRecipe(event.target.value);
+    };
+
+    const searchRecipe = async (toSearch: string) => {
+        const response = await bobFetch(`/api/recipes?s=${encodeURIComponent(toSearch)}`);
+        response.status == 200 ? setRecipes([...response.data]) : setError(response.data);
+    };
+
+    const goToRecipe = (recipeId: string) => {
+        router.push(`/recipe/${recipeId}`);
+    };
+
     return (
         <div id="searchModal" className="modal" tabIndex={-1}>
             <div className="modal-dialog modal-lg modal-fullscreen-md-down">
                 <div className="modal-content rounded-1">
                     <div className="modal-header">
-                        <input type="text" className="form-control me-1" placeholder="Rechercher une recette" v-model="search"/>
+                        <input type="text" value={search} onChange={handleChange} id="search-modal-input" className="form-control me-1" placeholder="Rechercher une recette"/>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body px-4" style={{ height: 750 }}>
-                        <div className="d-flex flex-row align-items-center" v-for="recipe in recipes">
-                            <Link className="fs-6 text-truncate my-auto py-2 text-decoration-none" href="#" data-bs-dismiss="modal">
-                                <span className="text-success me-3">{ recipe?.title }</span> { recipe?.ingredients }
-                            </Link>
-                        </div>
+                        {recipes.map((recipe, index) =>
+                            <div key={recipe.id} className={`d-flex flex-row align-items-center border-bottom${index == 0 ? " border-top" : ""}`}>
+                                <Link href="#" onClick={() => goToRecipe(recipe.id)} className=" col-12 fs-6 text-truncate my-auto py-2 text-decoration-none" data-bs-dismiss="modal">
+                                    <span className="text-success me-3">{ recipe?.title }</span> { recipe?.ingredients }
+                                </Link>
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
