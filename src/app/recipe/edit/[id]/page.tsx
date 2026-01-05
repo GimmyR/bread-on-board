4082214efea9858d1408b8f951ee2e96557809df
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-05 08:14:45 +0300

    Adding metadata

---
+++ src/app/recipe/edit/[id]/page.tsx
@@ -0,0 +1,25 @@
+import CreateRecipeForm from "@/components/create-recipe-form";
+import { RecipeResponse } from "@/interfaces/recipe";
+import bobFetch from "@/lib/bob-fetch";
+import { Metadata } from "next";
+
+type Props = {
+    params: Promise<{ id: string }>
+};
+
+export const metadata: Metadata = {
+    title: "Edit recipe - Bread on Board",
+    description: "Edit your recipe in Bread on Board web application.",
+};
+
+export default async function EditRecipePage({ params } : Props) {
+    const { id } = await params;
+    const responseRecipe = await bobFetch(`/api/recipes/${id}`);
+    const recipe: RecipeResponse | undefined = responseRecipe.status == 200 ? responseRecipe.data : undefined;
+
+    return (
+        <div className="container-fluid container-lg min-vh-100 bg-light py-5 mt-5">
+            <CreateRecipeForm recipe={recipe}/>
+        </div>
+    );
+}
