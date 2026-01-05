d1e8e0c94a7f68a06ada8d07cf9190166e8ee918
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-05 15:41:46 +0300

    Checking if authenticated before displaying page

--- src/app/my-recipes/page.tsx
+++ src/app/my-recipes/page.tsx
@@ -2,6 +2,7 @@ import { getToken } from "@/actions/get-token";
 import RecipeSection from "@/components/recipe-section";
 import bobFetch from "@/lib/bob-fetch";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "My recipes - Bread on Board",
@@ -9,13 +10,20 @@ export const metadata: Metadata = {
 };
 
 export default async function MyRecipes() {
+    const token = await getToken();
+
+    if(!token)
+        redirect("/sign-in");
+
     const response = await bobFetch("/api/my-recipes", { 
         headers: { 
-            "Authorization": `Bearer ${await getToken()}` 
+            "Authorization": `Bearer ${token}` 
         } 
     });
 
     return (
-        <RecipeSection recipes={response.data}/>
+        <>
+            {response.status == 200 && <RecipeSection recipes={response.data}/>}
+        </>
     );
 }
