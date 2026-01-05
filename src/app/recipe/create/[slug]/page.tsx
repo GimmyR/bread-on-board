f62d783572fc614ec9355e1f9047ed1cadf30091
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-05 07:23:48 +0300

    Creating create recipe form as a component in components repertory

--- src/app/recipe/create/[slug]/page.tsx
+++ src/app/recipe/create/[slug]/page.tsx
@@ -1,10 +1,10 @@
-import CreateRecipeForm from "./form";
+import CreateRecipeForm from "@/components/create-recipe-form";
 
 export default async function CreateRecipePage({ params } : { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
     const title = decodeURIComponent(slug);
     return (
-        <div className="container-fluid container-lg bg-light py-5 mt-5">
+        <div className="container-fluid container-lg min-vh-100 bg-light py-5 mt-5">
             <CreateRecipeForm title={title}/>
         </div>
     );
