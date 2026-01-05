d57b61e72d57260b8f3cf9266ab30deff7daf94a
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-05 15:12:18 +0300

    Creating function to easily get jwt (redirect to sign in page if cookie is undefined)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,7 @@
 import { jwtVerify } from "jose";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const secret = new TextEncoder().encode(process.env.JWT_SECRET);
 
@@ -11,4 +14,13 @@ export async function getSubjectFromToken(token: string | undefined) {
         
     } else return undefined;
 
+}
+
+export async function getToken() {
+    const cookieStore = await cookies();
+    const cookie: RequestCookie | undefined = cookieStore.get("token");
+
+    if(cookie)
+        return cookie.value;
+    else redirect("/sign-in");
 }
