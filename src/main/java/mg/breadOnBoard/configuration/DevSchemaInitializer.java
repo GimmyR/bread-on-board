098ee4094250ac91134187acbff48ba981cae006
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-06 22:40:02 +0300

    On dev environment, running DevSchemaInitializer. On prod, running prod-schema.sql. On test, running test-schema.sql

---
+++ src/main/java/mg/breadOnBoard/configuration/DevSchemaInitializer.java
@@ -0,0 +1,55 @@
+package mg.breadOnBoard.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+@Profile("dev")
+public class DevSchemaInitializer {
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	@PostConstruct
+	public void init() {
+		
+		jdbcTemplate.execute("""
+								BEGIN
+								  EXECUTE IMMEDIATE 'CREATE SEQUENCE ACCOUNT_SEQ START WITH 2 INCREMENT BY 1 NOORDER';
+								EXCEPTION
+								  WHEN OTHERS THEN
+								    IF SQLCODE != -955 THEN
+								      RAISE;
+								    END IF;
+								END;
+							""");
+		
+		jdbcTemplate.execute("""
+								BEGIN
+								  EXECUTE IMMEDIATE 'CREATE SEQUENCE RECIPE_SEQ START WITH 1 INCREMENT BY 1 NOORDER';
+								EXCEPTION
+								  WHEN OTHERS THEN
+								    IF SQLCODE != -955 THEN
+								      RAISE;
+								    END IF;
+								END;
+							""");
+		
+		jdbcTemplate.execute("""
+								BEGIN
+								  EXECUTE IMMEDIATE 'CREATE SEQUENCE RECIPE_STEP_SEQ START WITH 1 INCREMENT BY 1 NOORDER';
+								EXCEPTION
+								  WHEN OTHERS THEN
+								    IF SQLCODE != -955 THEN
+								      RAISE;
+								    END IF;
+								END;
+							""");
+		
+	}
+
+}
