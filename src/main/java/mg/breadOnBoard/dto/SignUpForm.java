66971e7d97cc220542f10f8afe7a0f447fdf16a3
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-06 21:26:40 +0300

    Putting mail address after username

--- src/main/java/mg/breadOnBoard/dto/SignUpForm.java
+++ src/main/java/mg/breadOnBoard/dto/SignUpForm.java
@@ -4,12 +4,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record SignUpForm(
+		@NotBlank(message = "Username is missing")
+		String username,
+		
 		@NotBlank(message = "Mail address is missing")
 		@Email(message = "Mail address is invalid")
 		String mailAddress,
-		
-		@NotBlank(message = "Username is missing")
-		String username,
 		
 		@NotBlank(message = "Password is missing")
 		String password
