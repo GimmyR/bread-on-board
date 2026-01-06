41a3d4083bcce39bff6174eb5bb94d5d8898d32d
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-06 11:46:23 +0300

    Listing all recipes

--- src/main/java/mg/breadOnBoard/controller/AdminController.java
+++ src/main/java/mg/breadOnBoard/controller/AdminController.java
@@ -3,11 +3,18 @@ package mg.breadOnBoard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.AllArgsConstructor;
 import mg.breadOnBoard.domain.BobView;
+import mg.breadOnBoard.model.Recipe;
+import mg.breadOnBoard.service.RecipeService;
 
 @Controller
+@AllArgsConstructor
 public class AdminController {
+	
+	private RecipeService recipeService;
 	
 	@GetMapping("/sign-in")
 	public String signInAsAdmin(Model model) {
@@ -17,9 +24,11 @@ public class AdminController {
 	}
 	
 	@GetMapping("/")
-	public String getHome(Model mode) {
+	public String getHome(Model model, @RequestParam(name = "s", required = false) String search) {
 		
-		return BobView.configure(mode, "home", "Home");
+		Iterable<Recipe> recipes = recipeService.findAll(search);
+		model.addAttribute("recipes", recipes);
+		return BobView.configure(model, "home", "Home");
 		
 	}
 
