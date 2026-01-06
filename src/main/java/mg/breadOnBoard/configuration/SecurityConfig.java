1412e378d4eaf7a2dc3fb7072fdd644df346456f
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-06 16:49:07 +0300

    Allowing to access to images without authentication

--- src/main/java/mg/breadOnBoard/configuration/SecurityConfig.java
+++ src/main/java/mg/breadOnBoard/configuration/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
 		httpSecurity
 				.formLogin(login -> login.loginPage("/sign-in").defaultSuccessUrl("/").permitAll())
 				.logout(logout -> logout.logoutSuccessUrl("/sign-in?logout").permitAll())
-				.authorizeHttpRequests(authorize -> authorize.requestMatchers("/webjars/**", "/css/**").permitAll())
+				.authorizeHttpRequests(authorize -> authorize.requestMatchers("/webjars/**", "/css/**", "/images/**").permitAll())
 				.authorizeHttpRequests(authorize -> authorize.requestMatchers("/**").hasRole("Admin"))
 				.userDetailsService(userDetailsService);
 		
