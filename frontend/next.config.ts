61ebfe75b87928f4a241016c462067609fb859f0
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-08 17:37:41 +0300

    Fixing .gitignore conflict

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
+const path = require("path");
+
+require("dotenv").config({
+    path: path.resolve(__dirname, '../.env')
+});
+
+module.exports = {
+    reactStrictMode: true
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
