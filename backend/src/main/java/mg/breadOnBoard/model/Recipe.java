2ccdd7fa59474aab6731d7f566a7060bcd6ee7e3
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-10 16:28:27 +0300

    Maximum length of ingredients = 500

--- backend/src/main/java/mg/breadOnBoard/model/Recipe.java
+++ backend/src/main/java/mg/breadOnBoard/model/Recipe.java
@@ -7,6 +7,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -42,6 +43,7 @@ public class Recipe {
 	
 	private String image;
 	
+	@Column(length = 500)
 	private String ingredients;
 	
 	@OneToMany(mappedBy = "recipe", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
