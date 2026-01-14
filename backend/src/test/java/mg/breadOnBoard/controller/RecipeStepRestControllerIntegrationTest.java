2864886208eae85ec78bceb842567c53bdc1c633
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-14 10:50:11 +0300

    Remove signUp, login and createRecipe method

--- backend/src/test/java/mg/breadOnBoard/controller/RecipeStepRestControllerIntegrationTest.java
+++ backend/src/test/java/mg/breadOnBoard/controller/RecipeStepRestControllerIntegrationTest.java
@@ -4,7 +4,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,19 +12,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import mg.breadOnBoard.dto.RecipeForm;
-import mg.breadOnBoard.dto.RecipeResponse;
 import mg.breadOnBoard.dto.RecipeStepForm;
-import mg.breadOnBoard.dto.SignUpForm;
 import mg.breadOnBoard.dto.StepsForm;
+import mg.breadOnBoard.model.Recipe;
+import mg.breadOnBoard.service.RecipeService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,70 +37,34 @@ public class RecipeStepRestControllerIntegrationTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 	
+	@Autowired
+	private RecipeService recipeService;
+	
 	@Test
+	@WithMockUser(username = "adawong")
 	public void testSaveAll() throws JsonProcessingException, Exception {
 		
-		MvcResult result = this.signUp();
-		MvcResult result2 = this.createRecipe(result);
-		this.saveSteps(result, result2);
+		Iterable<Recipe> recipes = recipeService.findAll(null);
 		
-	}
-	
-	private MvcResult saveSteps(MvcResult result, MvcResult result2) throws JsonProcessingException, UnsupportedEncodingException, Exception {
-		
-		RecipeResponse recipe = objectMapper.readValue(result2.getResponse().getContentAsString(), RecipeResponse.class);
 		List<RecipeStepForm> steps = new ArrayList<RecipeStepForm>();
 		steps.add(new RecipeStepForm("Break an egg and put the contents in a bowl."));
 		steps.add(new RecipeStepForm("Do it for the second."));
-		StepsForm form = new StepsForm(recipe.id(), steps);
+		StepsForm form = new StepsForm(recipes.iterator().next().getId(), steps);
 		
-		return mockMvc.perform(post("/api/recipe-steps/save")
-				.header("Authorization", "Bearer " + result.getResponse().getContentAsString())
+		mockMvc.perform(post("/api/recipe-steps/save")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(objectMapper.writeValueAsBytes(form)))
-				.andExpect(status().isCreated())
-				.andReturn();
-		
-	}
-	
-	private MvcResult signUp() throws JsonProcessingException, Exception {
-		
-		return mockMvc.perform(post("/api/sign-up")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsBytes(new SignUpForm("johndoe", "johndoe@example.com", "pwdJohn"))))
-				.andExpect(status().isCreated())
-				.andReturn();
-		
-	}
-	
-	private MvcResult createRecipe(MvcResult result) throws JsonProcessingException, UnsupportedEncodingException, Exception {
-		
-		return mockMvc.perform(post("/api/recipe/create")
-				.header("Authorization", "Bearer " + result.getResponse().getContentAsString())
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsBytes(new RecipeForm("Omelette", "2 Eggs, oil, salt"))))
-				.andExpect(status().isCreated())
-				.andReturn();
+				.andExpect(status().isCreated());
 		
 	}
 	
 	@Test
 	public void testGetAllByRecipeId() throws Exception {
 		
-		MvcResult result = this.signUp();
-		MvcResult result2 = this.createRecipe(result);
-		MvcResult result3 = this.saveSteps(result, result2);
-		this.getAllByRecipeId(result3);
+		Iterable<Recipe> recipes = recipeService.findAll(null);
 		
-	}
-	
-	private MvcResult getAllByRecipeId(MvcResult result) throws Exception {
-		
-		RecipeResponse recipe = objectMapper.readValue(result.getResponse().getContentAsString(), RecipeResponse.class);
-		
-		return mockMvc.perform(get("/api/recipe-steps/" + recipe.id()))
-				.andExpect(status().isOk())
-				.andReturn();
+		mockMvc.perform(get("/api/recipe-steps/" + recipes.iterator().next().getId()))
+			.andExpect(status().isOk());
 		
 	}
 
