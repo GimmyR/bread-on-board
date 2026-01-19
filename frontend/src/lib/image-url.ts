73c597cfe3bf94eb836a4a45970f2a4f4f4e4e11
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-19 16:08:52 +0300

    Use image url in frontend if not undefined

--- frontend/src/lib/image-url.ts
+++ frontend/src/lib/image-url.ts
@@ -1,5 +1,7 @@
-import { IMAGE_URL } from "./api";
+import { CLIENT_SIDE_TO_API, IMAGE_URL } from "./api";
 
 export default function imageURL(filename: string) {
-    return `${IMAGE_URL}/${filename}`;
+    if(IMAGE_URL)
+        return `${IMAGE_URL}/${filename}`;
+    else return `${CLIENT_SIDE_TO_API}/images/${filename}`;
 }
