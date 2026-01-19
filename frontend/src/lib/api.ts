f1ef0c9a07c80ea0e2470e8ee5f265a29009bd9d
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2026-01-19 12:23:45 +0300

    Export image url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,2 +1,3 @@
 export const SERVER_SIDE_TO_API = process.env.NEXT_PUBLIC_SERVER_SIDE_TO_API;
 export const CLIENT_SIDE_TO_API = process.env.NEXT_PUBLIC_CLIENT_SIDE_TO_API;
+export const IMAGE_URL = process.env.NEXT_PUBLIC_IMAGE_URL;
